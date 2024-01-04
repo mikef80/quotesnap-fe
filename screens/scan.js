51011af27e918b6532b0bf4c1795c9a4d3feb3e6
Mike <mike@mike-francis.org>
2024-01-04 16:09:18 +0000

    scanning is working from picking an image.  Displaying the quote on the screen, just need to sort otu saving it to the database

--- screens/scan.js
+++ screens/scan.js
@@ -1,16 +1,121 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
 import Navigation from "../components/Navigation";
+import * as ImagePicker from "expo-image-picker";
+import MlkitOcr from "react-native-mlkit-ocr";
+import { useEffect, useState } from "react";
 
 export default function Scan() {
+  const [image, setImage] = useState(null);
+  const [text, setText] = useState(null);
+
+  /* const openCamera = async () => {
+    const permission = await ImagePicker.requestCameraPermissionsAsync();
+    console.log(permission);
+
+    if (!permission.granted) {
+      alert("You have denied permission to the camera");
+      return;
+    }
+
+    const result = await ImagePicker.launchCameraAsync();
+    console.log(result);
+
+    const uri = result.assets[0].uri;
+
+    setImage(uri);
+
+    const resultText = await MlkitOcr.detectFromUri(uri);
+
+    setText(resultText[0]);
+    console.log(text);
+  }; */
+
+  const pickImage = async () => {
+    const permission = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!permission.granted) {
+      alert("You have denied permission to the camera");
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      quality: 1,
+      allowsEditing: true,
+    });
+
+    const uri = result.assets[0].uri;
+
+    setImage(uri);
+
+    const resultFromUri = await MlkitOcr.detectFromFile(uri);
+
+    if (resultFromUri?.length > 0) {
+      let _ = resultFromUri.map((line) => line.text);
+      console.log(_, "<-- _");
+      _ = JSON.stringify(_.join(" ").replaceAll("\n", ' '))
+      _ = _.replaceAll("\\", " ")
+      setText(_)
+      // setText(JSON.stringify(_.join(" ").replaceAll("\n", " ")));
+    }
+  };
+
+  useEffect(() => {
+    if (text) {
+      console.log(text, "<-- text elements");
+    }
+  }, [text]);
+
+  useEffect(() => {
+    if (image) {
+      console.log(image, "<-- image");
+    }
+  }, [image]);
+
   return (
-    <View>
-      <Text>Scan Page</Text>
+    <View style={styles.Main}>
+      <View>
+        {/* <Button title='Scan quote' onPress={openCamera} /> */}
+        {text && image && (
+          <View>
+            <Image source={{ uri: image }} style={styles.Image} />
+            <Text style={styles.Text}>{text}</Text>
+          </View>
+        )}
+        <View style={{ alignSelf: "center" }}>
+          <View style={{ width: "50%" }}>
+            <Button
+              style={styles.Button}
+              title='Pick Image'
+              onPress={pickImage}
+            />
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  NavigationContainer: {
-    backgroundColor: "yellow",
+  Text: {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 20,
+    margin: 20,
+  },
+  Image: {
+    width: window.width,
+    height: 300,
+    objectFit: "contain",
+    padding: 20,
+    margin: 20,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  Button: {
+    backgroundColor: "red",
+  },
+  Main: {
+    flexDirection: "column",
+    alignItems: "center",
   },
 });
