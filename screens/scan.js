54609453853f32fcbd8af7acff8c015544356fab
jamiemharper98 <jamiemharper98@gmail.com>
2024-01-08 13:37:34 +0000

    scan information options

--- screens/scan.js
+++ screens/scan.js
@@ -1,20 +1,24 @@
-import {
-  Button,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  ScrollView,
-} from "react-native";
+import { Button, StyleSheet, Text, View, Image, ScrollView, Dimensions } from "react-native";
 import Navigation from "../components/Navigation";
 import * as ImagePicker from "expo-image-picker";
 import MlkitOcr from "react-native-mlkit-ocr";
 import { useEffect, useState } from "react";
 import { postNewQuote } from "../api/api";
+import { useUserContext } from "../Contexts/UserContext";
+import Login from "./login";
+import { TextInput } from "react-native-gesture-handler";
+import { Checkbox, RadioButton } from "react-native-paper";
 
 export default function Scan() {
   const [image, setImage] = useState(null);
   const [text, setText] = useState(null);
+  const [author, setAuthor] = useState("");
+  const [origin, setOrigin] = useState("");
+  const [isPrivate, setIsPrivate] = useState(false);
+  const [category, setCategory] = useState("");
+  const { user } = useUserContext();
+  const height = Dimensions.get("window").height;
+  const width = Dimensions.get("window").width;
 
   const openCamera = async () => {
     let permission = await ImagePicker.requestCameraPermissionsAsync();
@@ -77,61 +81,124 @@ export default function Scan() {
     console.log("saving quote...");
     const quoteToSave = {
       quoteText: text,
-      quoteAuthor: "LinkedIn", // freetext
-      quoteOrigin: "fiction book", // freetext
-      quoteLocation: "[10, 10]",
+      quoteAuthor: author || "",
+      quoteOrigin: origin || "",
+      quoteLocation: "[51, 0]",
       quoteImage: image,
-      quoteIsPrivate: false, // boolean
-      quoteCategory: "Billboard", // freetext
-      quoteUser: "Hello",
+      quoteIsPrivate: !isPrivate,
+      quoteCategory: category || "Misc.",
+      quoteUser: user?.username || "Hello",
     };
     postNewQuote(quoteToSave).then((returnedQuote) => {
       console.log("Quote save");
       setText(null);
       setImage(null);
+      setAuthor("");
+      setOrigin("");
+      setIsPrivate(false);
+      setCategory("");
     });
   };
+
+  function handleAuthor(text) {
+    setAuthor(text);
+  }
+
+  function handleOrigin(text) {
+    setOrigin(text);
+  }
+
+  function handleCategory(text) {
+    setCategory(text);
+  }
+
+  if (!user) {
+    return (
+      <View>
+        <Login />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.Main}>
       <ScrollView>
         {text && image && (
-          <View>
-            <Image source={{ uri: image }} style={styles.Image} />
-            <Text style={styles.Text}>{text}</Text>
-          </View>
-        )}
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            justifyContent: "space-around",
-            alignItems: "center",
-          }}>
-          <View>
-            <Button
-              style={styles.Button}
-              title='Pick Image'
-              onPress={pickImage}
-            />
-          </View>
-          <View>
-            <Button
-              style={styles.Button}
-              title='Scan quote'
-              onPress={openCamera}
-            />
-          </View>
-          {text && image && (
+          <>
             <View>
-              <Button
-                style={styles.Button}
-                title='Save Scan'
-                onPress={saveScan}
+              <Image source={{ uri: image }} style={styles.Image} />
+              <View style={styles.form}>
+                <Text>Quote Text</Text>
+              </View>
+              <TextInput
+                multiline
+                style={styles.Text}
+                value={text}
+                onChangeText={(text) => {
+                  setText(text);
+                }}
               />
             </View>
-          )}
-        </View>
+
+            <View style={styles.form}>
+              <Text>Author</Text>
+              <TextInput style={styles.InputBox} value={author} onChangeText={handleAuthor} />
+
+              <Text>Origin</Text>
+              <TextInput style={styles.InputBox} value={origin} onChangeText={handleOrigin} />
+
+              <Text>Is Private?</Text>
+              <Checkbox
+                status={!isPrivate ? "checked" : "unchecked"}
+                onPress={() => {
+                  setIsPrivate((prevVal) => {
+                    return !prevVal;
+                  });
+                }}
+              />
+              <Text>Category</Text>
+              <TextInput style={styles.InputBox} value={category} onChangeText={handleCategory} />
+            </View>
+          </>
+        )}
+
+        {!text && !image && (
+          <View
+            style={{
+              height: height - 150,
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <View>
+              <Button color={"#5DB075"} title="Pick Image" onPress={pickImage} />
+            </View>
+            <View style={{ paddingLeft: 20 }}>
+              <Button color={"#5DB075"} title="Scan quote" onPress={openCamera} />
+            </View>
+          </View>
+        )}
+        {text && image && (
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+          >
+            <View>
+              <Button color={"#5DB075"} title="Pick Image" onPress={pickImage} />
+            </View>
+            <View style={{ paddingLeft: 20 }}>
+              <Button color={"#5DB075"} title="Scan quote" onPress={openCamera} />
+            </View>
+            <View style={{ paddingLeft: 20 }}>
+              <Button color={"#5DB075"} title="Save Scan" onPress={saveScan} />
+            </View>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
@@ -141,20 +208,29 @@ const styles = StyleSheet.create({
   Text: {
     borderWidth: 1,
     borderColor: "black",
-    padding: 20,
-    margin: 20,
+    padding: 10,
+    margin: 5,
   },
   Image: {
-    width: window.width,
+    // width: window.width,
     height: 300,
     objectFit: "contain",
     padding: 20,
-    margin: 20,
+    margin: 5,
     borderWidth: 1,
     borderColor: "black",
   },
   Main: {
     flexDirection: "column",
     alignItems: "center",
+  },
+  InputBox: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 5,
+    marginTop: 5,
+  },
+  form: {
+    margin: 5,
   },
 });
